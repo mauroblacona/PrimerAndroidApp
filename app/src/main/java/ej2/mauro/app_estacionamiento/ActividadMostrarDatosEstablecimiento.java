cdbdb45bff906da0fb6229380a885fbcfd03683c
Maurodsw <blaconamauro@gmail.com>
2018-11-02 12:03:11 -0300

    Version 1 del proyecto se termino actividad mostrar semaforo y se agrego gps, falta adaptar el mapa

--- app/src/main/java/ej2/mauro/app_estacionamiento/ActividadMostrarDatosEstablecimiento.java
+++ app/src/main/java/ej2/mauro/app_estacionamiento/ActividadMostrarDatosEstablecimiento.java
@@ -1,7 +1,10 @@
 package ej2.mauro.app_estacionamiento;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
 
 import java.text.SimpleDateFormat;
@@ -14,6 +17,25 @@ public class ActividadMostrarDatosEstablecimiento extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_actividad_mostrar_datos_establecimiento);
         mostrarDatos();
+        activarBotones();
+    }
+
+    private void activarBotones() {
+        Button btnVolver = (Button) findViewById(R.id.btnVolver);
+        btnVolver.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                onBackPressed();
+            }
+        });
+        Button btnIrgps = (Button) findViewById(R.id.btnIrgps);
+        btnIrgps.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent mostrarGps = new Intent(ActividadMostrarDatosEstablecimiento.this, ActividadMostrarGps.class);
+                startActivity(mostrarGps);
+            }
+        });
     }
 
     private void mostrarDatos() {
