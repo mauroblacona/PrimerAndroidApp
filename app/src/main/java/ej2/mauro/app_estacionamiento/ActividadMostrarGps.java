ba68d0018aef252a5b478ffdf291fd0ac7fe4d3b
Maurodsw <blaconamauro@gmail.com>
2018-11-02 21:54:21 -0300

    Version 1 del proyecto se termino actividad de google maps satisfactoriamente

--- app/src/main/java/ej2/mauro/app_estacionamiento/ActividadMostrarGps.java
+++ app/src/main/java/ej2/mauro/app_estacionamiento/ActividadMostrarGps.java
@@ -1,12 +1,17 @@
 package ej2.mauro.app_estacionamiento;
 
+import android.app.Activity;
+import android.app.Dialog;
 import android.support.v4.app.FragmentActivity;
 import android.os.Bundle;
 
+import com.google.android.gms.common.ConnectionResult;
+import com.google.android.gms.common.GooglePlayServicesUtil;
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.UiSettings;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.MarkerOptions;
 
@@ -19,28 +24,43 @@ public class ActividadMostrarGps extends FragmentActivity implements OnMapReadyC
         super.onCreate(savedInstanceState);
         setContentView(R.layout.actividad_mostrar_gps);
         // Obtain the SupportMapFragment and get notified when the map is ready to be used.
-        SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
-                .findFragmentById(R.id.map);
-        mapFragment.getMapAsync(this);
+
+        int status = GooglePlayServicesUtil.isGooglePlayServicesAvailable(getApplicationContext());
+
+        if (status == ConnectionResult.SUCCESS) {
+            SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
+                    .findFragmentById(R.id.map);
+            mapFragment.getMapAsync(this);
+        } else {
+            Dialog dialog = GooglePlayServicesUtil.getErrorDialog(status, (Activity) getApplicationContext(), 10);
+            dialog.show();
+        }
     }
 
 
-    /**
-     * Manipulates the map once available.
-     * This callback is triggered when the map is ready to be used.
-     * This is where we can add markers or lines, add listeners or move the camera. In this case,
-     * we just add a marker near Sydney, Australia.
-     * If Google Play services is not installed on the device, the user will be prompted to install
-     * it inside the SupportMapFragment. This method will only be triggered once the user has
-     * installed Google Play services and returned to the app.
-     */
-    @Override
-    public void onMapReady(GoogleMap googleMap) {
-        mMap = googleMap;
+        /**
+         * Manipulates the map once available.
+         * This callback is triggered when the map is ready to be used.
+         * This is where we can add markers or lines, add listeners or move the camera. In this case,
+         * we just add a marker near Sydney, Australia.
+         * If Google Play services is not installed on the device, the user will be prompted to install
+         * it inside the SupportMapFragment. This method will only be triggered once the user has
+         * installed Google Play services and returned to the app.
+         */
+        @Override
+        public void onMapReady (GoogleMap googleMap){
+            mMap = googleMap;
 
-        // Add a marker in Sydney and move the camera
-        LatLng sydney = new LatLng(-34, 151);
-        mMap.addMarker(new MarkerOptions().position(sydney).title("Marker in Sydney"));
-        mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney));
+            mMap.setMapType(GoogleMap.MAP_TYPE_NORMAL);
+
+            UiSettings uiSettings = mMap.getUiSettings();
+            uiSettings.setZoomControlsEnabled(true);
+
+            // Add a marker in Sydney and move the camera
+            LatLng santafe = new LatLng(-31.649175, -60.700873);
+            mMap.addMarker(new MarkerOptions().position(santafe).title("Casino de Santa Fe - Donde Estaciono?"));
+            float zoomlevel=16;
+            mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(santafe, zoomlevel));
+        }
     }
-}
+
